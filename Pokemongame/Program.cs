8d3c04ea8b1a0803da10dde74731d56ba64ddb72
wobunto <ifsidayuri@naver.com>
2026-09-05 04:48:07 +0000

    수정

--- Pokemongame/Program.cs
+++ Pokemongame/Program.cs
@@ -4,30 +4,7 @@ namespace MyGame
     class Program{
         static void Main(string[] args)
         {
-            PokemonDatabase.LoadPokemonDatabase();
-            MoveDatabase.LoadMoveDatabase();
-            
-            BattleSystem battle = new BattleSystem();
-            
-            var playerRuntime = new PlayerRuntime();
-            var enemyRuntime = new EnemyRuntime();
-
-            
-
-            var rizard = PokemonFactory.Create(6 ,50);
-            rizard.TryLearn(102);
-            rizard.TryLearn(105);
-            rizard.TryLearn(106);
-            rizard.TryLearn(107);
-
-            var laflas = PokemonFactory.Create(3,50);
-            laflas.TryLearn(101);
-            laflas.TryLearn(103);
-            
-            playerRuntime.CapturePokemon(rizard);
-            enemyRuntime.CapturePokemon(laflas);
-                
-            battle.StartBattle(playerRuntime,enemyRuntime);
+          
        }
     }
 }
